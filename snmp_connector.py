761fa61a412e5d2027d77c54b5d16d7dfaa06bee
Arapbaev Malik <arapbaevmalik2002@gmail.com>
2025-02-16 17:45:54 +0500

    check for oids

--- snmp_connector.py
+++ snmp_connector.py
@@ -1,22 +1,33 @@
 import asyncio
-from pysnmp.hlapi.v3arch.asyncio import SnmpEngine, CommunityData, UdpTransportTarget, \
-    ContextData, ObjectType, ObjectIdentity, get_cmd
+from pysnmp.hlapi.v3arch.asyncio import SnmpEngine, CommunityData, next_cmd,\
+    UdpTransportTarget, ContextData, ObjectType, ObjectIdentity, get_cmd
 
 COMMUNITY = "huawei_router"
 
-async def main(oid):
+async def main(oid, com):
     # Await the UDP target creation
     udp_target = await UdpTransportTarget.create(('192.168.10.100', 161), timeout=1, retries=5, tagList='')
+    snmp_engine = SnmpEngine()
+    auth = CommunityData(COMMUNITY, mpModel=1)
 
-    # Await the get_cmd coroutine
-    result = await get_cmd(
-        SnmpEngine(),
-        CommunityData(COMMUNITY, mpModel=1),
-        udp_target,
-        ContextData(),
-        ObjectType(ObjectIdentity(oid))
-    )
-
+    if com == 0:
+        # Await the get_cmd coroutine
+        result = await get_cmd(
+            snmp_engine,
+            auth,
+            udp_target,
+            ContextData(),
+            ObjectType(ObjectIdentity(oid))
+        )
+    else:
+        result = next_cmd(
+            snmp_engine,
+            auth,
+            udp_target,
+            ContextData(),
+            ObjectType(ObjectIdentity(oid)),
+            lexicographicMode=False  # Stops at the end of subtree
+        )
     # Unpack the result tuple
     errorIndication, errorStatus, errorIndex, varBinds = result
 
@@ -26,9 +37,9 @@ async def main(oid):
         print(f"{errorStatus.prettyPrint()} at {errorIndex and varBinds[int(errorIndex) - 1][0] or '?'}")
     else:
         for oid, val in varBinds:
-            print(f'{oid.prettyPrint()} = {decode_sysdescr(val.prettyPrint())}')
+            print(f'{oid.prettyPrint()} = {decode_hex(val.prettyPrint())}')
 
-def decode_sysdescr(sysdescr):
+def decode_hex(sysdescr):
     # Check if the string is hex-encoded (common if it starts with "0x")
     if sysdescr.startswith("0x") or sysdescr.startswith("0X"):
         # Remove the leading "0x"
@@ -43,9 +54,29 @@ def decode_sysdescr(sysdescr):
         # If it's not hex, return the original string
         return sysdescr
 
-
+'''
 asyncio.run(main('1.3.6.1.2.1.1.1.0'))
 asyncio.run(main('1.3.6.1.2.1.1.2.0'))
 asyncio.run(main('1.3.6.1.2.1.1.3.0'))
 asyncio.run(main('1.3.6.1.2.1.1.4.0'))
 asyncio.run(main('1.3.6.1.2.1.1.5.0'))
+'''
+
+# LLDP (1.0.8802.1.1.2.1.4.1)
+#asyncio.run(main("1.0.8802.1.1.2.1.4.1.1"))
+#asyncio.run(main("1.0.8802.1.1.2.1.4.1.2"))
+asyncio.run(main("1.3.6.1.2.1.17.2.4.1"))
+
+# ARP (1.3.6.1.2.1.4.22.1)
+#asyncio.run(main("1.3.6.1.2.1.4.22.1.2"))
+#asyncio.run(main("1.3.6.1.2.1.4.22.1.3"))
+asyncio.run(main("1.3.6.1.2.1.3.1.1.2"))
+asyncio.run(main("1.3.6.1.2.1.3.1.1.3"))
+
+# Routing (1.3.6.1.2.1.4.24.4.1)
+#asyncio.run(main("1.3.6.1.2.1.4.24.4.1.1"))
+#asyncio.run(main("1.3.6.1.2.1.4.24.4.1.2"))
+#asyncio.run(main("1.3.6.1.2.1.4.24.4.1.3"))
+asyncio.run(main("1.3.6.1.2.1.4.21.1.1"))
+asyncio.run(main("1.3.6.1.2.1.4.21.1.2"))
+asyncio.run(main("1.3.6.1.2.1.4.21.1.3"))
